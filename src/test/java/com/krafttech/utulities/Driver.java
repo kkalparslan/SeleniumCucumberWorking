109ec522f20dd2a94778c60cb749d6d41911e6fd
Alparslan <alparslanztk@gmail.com>
2024-02-24 13:21:59 +0300

    24.02.2024 edited Driver class and pom.xml

--- src/test/java/com/krafttech/utulities/Driver.java
+++ src/test/java/com/krafttech/utulities/Driver.java
@@ -1,5 +1,4 @@
 package com.krafttech.utulities;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,41 +22,41 @@ public class Driver {
             String browser = ConfigurationReader.get("browser");
             switch (browser) {
                 case "chrome":
-                    WebDriverManager.chromedriver().setup();
+                   // WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     break;
                 case "chrome-headless":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                  //  WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
                     break;
 
                 case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
+                  //  WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
                 case "firefox-headless":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                  //  WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(new FirefoxOptions().addArguments("--headless"));
                     break;
 
                 case "ie":
                     if (!System.getProperty("os.name").toLowerCase().contains("windows"))
                         throw new WebDriverException("Your OS doesn't support Internet Explorer");
-                    WebDriverManager.iedriver().setup();
+                  //  WebDriverManager.iedriver().setup();
                     driver = new InternetExplorerDriver();
                     break;
 
                 case "edge":
                     if (!System.getProperty("os.name").toLowerCase().contains("windows"))
                         throw new WebDriverException("Your OS doesn't support Edge");
-                    WebDriverManager.edgedriver().setup();
+                  //  WebDriverManager.edgedriver().setup();
                     driver = new EdgeDriver();
                     break;
 
                 case "safari":
                     if (!System.getProperty("os.name").toLowerCase().contains("mac"))
                         throw new WebDriverException("Your OS doesn't support Safari");
-                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                  //  WebDriverManager.getInstance(SafariDriver.class).setup();
                     driver = new SafariDriver();
                     break;
             }
